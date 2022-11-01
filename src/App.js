fa2adb9b42df8ce043bb3533044be7ac84999d67
Matthew Baillie <mbaillie93@gmail.com>
2022-11-01 13:54:45 -0700

    test and code button click

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [buttonColor, setButtonColor] = useState("red");
+  const newButtonColor = buttonColor === "red" ? "blue" : "red";
+
+  const changeButtonColor = () => {
+    setButtonColor(newButtonColor);
+  };
   return (
     <div>
-      <button style={{ backgroundColor: "red" }}>Change to blue</button>
+      <button
+        onClick={changeButtonColor}
+        style={{ backgroundColor: buttonColor }}
+      >
+        Change to {newButtonColor}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("button has correct initial colour", () => {
+test("button has correct initial colour, and updates when clicked", () => {
   render(<App />);
   // find an element with a role of button and text of "change to blue"
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
-});
 
-test("button turns blue when clicked", () => {
-  render(<App />);
+  //click button
+  fireEvent.click(colorButton);
+  // expect the background-color of blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  //Expect the button text to be "Change to red"
+  expect(colorButton).toHaveTextContent("Change to red");
 });
